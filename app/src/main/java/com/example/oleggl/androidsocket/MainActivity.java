0977c6552994b8f563fdc0bc9e113662256367e6
Oleg Golosovskiy <oleggl@steppechange.com>
2018-01-24 17:29:19 +0300

    initial

---
+++ app/src/main/java/com/example/oleggl/androidsocket/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.oleggl.androidsocket;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.util.Log;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.net.SocketOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button socket_button;
+
+    static void stringToPacket(String s, DatagramPacket packet) {
+        byte[] bytes = s.getBytes();
+        System.arraycopy(bytes, 0, packet.getData(), 0, bytes.length);
+        packet.setLength(bytes.length);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        socket_button =  (Button) findViewById(R.id.btn_socket);
+        socket_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("SocketTest", "button pressed ");
+
+                byte[] buffer = new byte[256];
+                DatagramSocket socket = null;
+
+                try {
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+                    InetAddress addr = InetAddress.getByName("77.120.105.29");
+
+                    socket = new DatagramSocket(); // 0, InetAddress.getLocalHost());
+
+                    socket.setOption(SocketOptions.IP_TOS, new Integer(0x22)  );
+
+                    String s = "Hello, Android";
+                    stringToPacket(s, packet);
+                    packet.setAddress(addr);
+                    packet.setPort(7777);
+
+                    socket.send(packet);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally
+                {
+                    if (socket != null) {
+                        socket.close();
+                    }
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
